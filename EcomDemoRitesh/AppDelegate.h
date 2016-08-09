7eda9c89f68db0d263f819fe78d9aa8c56626644
riteshmiimobileapp <riteshsharma06@gmail.com>
2016-08-09 20:15:26 +0530

    Initial Commit

---
+++ EcomDemoRitesh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EcomDemoRitesh
+//
+//  Created by SoftBunch  on 8/9/16.
+//  Copyright © 2016 miimobileapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EcomDemoRitesh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EcomDemoRitesh
+//
+//  Created by SoftBunch  on 8/9/16.
+//  Copyright © 2016 miimobileapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
